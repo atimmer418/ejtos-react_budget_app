923eae5c109366719ce8827e8dbd2ab37cab6e25
Andrew Timmer <atimmer@terpmail.umd.edu>
2023-12-29 15:42:48 -0500

    budget allocation app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,19 @@
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleChange = (e) => {
+        setNewBudget(e.target.value);
+    }
+    
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: ${budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleChange}/>
+        </div>
+    );
+}
+
+export default Budget;
